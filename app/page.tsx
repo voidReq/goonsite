c256e41397857b1277113a687426fbced67b50bf
voidReq <jhanstedtas@gmail.com>
2025-01-06 09:12:10 -0600

    changed goondata

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <MantineProvider forceColorScheme = "dark">
       
-          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column'}}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column'}}>
 
 
         <Image
