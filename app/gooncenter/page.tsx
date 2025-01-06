93a3533d063ba9aa4ca6e89d559122969e6a4df6
voidReq <jhanstedtas@gmail.com>
2025-01-06 09:10:02 -0600

    added lebron

--- app/gooncenter/page.tsx
+++ app/gooncenter/page.tsx
@@ -1,10 +1,20 @@
 "use client";
-import { MantineProvider, Text } from '@mantine/core';
+import { MantineProvider, Text, Image } from '@mantine/core';
 
 export default function GoonCenter() {
   return (
     <MantineProvider forceColorScheme='dark'>
-      <Text size="xl">Welcome to the Goon Center!</Text>
+      
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column'}}>
+      <Text size="xl">Welcome to the Goon Center</Text>
+      <Image
+          radius="md"
+          h={1000}
+          w={1000}
+          fit="contain"
+          src="/lebron.png"
+        />
+        </div>
     </MantineProvider>
   );
 } 
