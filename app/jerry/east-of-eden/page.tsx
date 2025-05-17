024b7c9af9c31b5136a0c7ace49f493e3a22b348
Junhao Zhang <jerry_mountain@hotmail.com>
2025-05-16 23:36:23 -0500

    finish done

--- app/jerry/east-of-eden/page.tsx
+++ app/jerry/east-of-eden/page.tsx
@@ -7,6 +7,7 @@ const posts = getPosts("east-of-eden");
 export default function EastOfEdenPage() {
     return (
         <LandingPageComponent
+            imgSrc="https://cdn.clearcreekresources.org/wp-content/uploads/2022/02/MicrosoftTeams-image-26-scaled.jpg"
             folder="east-of-eden"
             title="East of Eden"
             desc={`East of Eden is a book written by John Steinbeck. The book explores the intertwining history of two families, the Trasks and the Hamiltons, in California's Salinas Valley.
--- app/jerry/page.tsx
+++ app/jerry/page.tsx
@@ -10,25 +10,25 @@ const links = [
         href: "/jerry/east-of-eden",
         label: "East of Eden",
         description: "Reflections on Steinbeck's magnum opus.",
-        img: "https://cdn.clearcreekresources.org/wp-content/uploads/2022/02/MicrosoftTeams-image-26-scaled.jpg", // Garden/tree image
+        img: "https://cdn.clearcreekresources.org/wp-content/uploads/2022/02/MicrosoftTeams-image-26-scaled.jpg",
     },
     {
         href: "/jerry/non-fiction",
         label: "The Cheating Culture",
         description: "Dialectical journal about ideas presented in the book, the cheating culture of America, and how more people are cheating.",
-        img: "https://m.media-amazon.com/images/I/61zwec5kIIL.jpg", // Books image
+        img: "https://m.media-amazon.com/images/I/61zwec5kIIL.jpg",
     },
     {
         href: "/jerry/short-non-fiction",
         label: "Short Non Fiction",
         description: "Reflections on short non-fiction pieces. Also includes my reflections on studying for the AP Lang test.",
-        img: "https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=400&q=80", // Notepad image
+        img: "https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=400&q=80",
     },
     {
         href: "/jerry/painter-thief",
         label: "The Painter and the Thief",
         description: "A documentary about a relationship that develops between a painter and the thief that stole her artwork.",
-        img: "https://platform.vox.com/wp-content/uploads/sites/2/chorus/uploads/chorus_asset/file/19992699/painter1.jpg?quality=90&strip=all&crop=7.8125,0,84.375,100", // Film/camera image
+        img: "https://platform.vox.com/wp-content/uploads/sites/2/chorus/uploads/chorus_asset/file/19992699/painter1.jpg?quality=90&strip=all&crop=7.8125,0,84.375,100",
     }
 ];
 
